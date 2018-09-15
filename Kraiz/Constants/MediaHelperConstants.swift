74a30d2ac941a1a00a4238c36f6e2719df9849a9
Kanhai Agarwal <kanhaiagarwal@gmail.com>
2018-09-15 20:51:26 +0530

    Adding functionality of download and upload image, along with update profile

---
+++ Kraiz/Constants/MediaHelperConstants.swift
@@ -0,0 +1,15 @@
+//
+//  MediaManagerConstants.swift
+//  Kraiz
+//
+//  Created by Kumar Agarwal, Kanhai on 15/09/18.
+//  Copyright © 2018 Kumar Agarwal, Kanhai. All rights reserved.
+//
+
+import Foundation
+
+class MediaManagerConstants {
+    public static let CLOUD_NAME = "dsrlkfbl2"
+    public static let API_KEY = "943593667142819"
+    public static let API_SECRET = "aHfFosh2cm3GJmYypZz5CnkAVrQ"
+}
---
+++ Kraiz/Controller/Network/MediaHelper.swift
@@ -0,0 +1,9 @@
+//
+//  MediaHelper.swift
+//  Kraiz
+//
+//  Created by Kumar Agarwal, Kanhai on 15/09/18.
+//  Copyright © 2018 Kumar Agarwal, Kanhai. All rights reserved.
+//
+
+import Foundation
